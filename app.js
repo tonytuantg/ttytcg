b9523d815c06e8c8890bd3ca35f3273b9e5502bf
tony tuan <tuantvp@gmail.com>
2022-10-20 16:06:06 +0700

    upload google api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const { google } = require('googleapis');
 // var session = require('express-session');
 
 var indexRouter = require('./routes/index');
