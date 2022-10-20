e85ede945ef4242fafe78f35a9cac9b91a28bc33
tony tuan <tuantvp@gmail.com>
2022-10-20 16:19:52 +0700

    upload login

--- routes/login.js
+++ routes/login.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
      // res.send('hello');
      // const dateString = new Date();
      // const sheetTitle = dateString.getFullYear();
-     res.send({"kq" : getDataApiGgl.kq('taikhoan_khoaphong')}) ;
+     res.send({"kq" : 'hello'}) ;
 });	
 
 router.post('/check_loginServer', cors(), async function(req, res, next) {
