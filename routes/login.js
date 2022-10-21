8a5e5d535fcbbda354c0da8b825798de5f23dd18
tony tuan <tuantvp@gmail.com>
2022-10-21 22:52:07 +0700

    upload heroku

--- routes/login.js
+++ routes/login.js
@@ -29,30 +29,44 @@ router.get('/', async function(req, res, next) {
 });	
 
 router.post('/check_loginServer', cors(), async function(req, res, next) {
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     next();
+     // res.header("Access-Control-Allow-Origin", "*");
+     // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     // next();
 
-     const trangthai = 0;
-     const tenTaiKhoan= '';
-     const username = req.body.username;
-     const password = req.body.password;
+     var trangthai = 0;
+     var tenTaiKhoan= 'd';
+     var kq = '';
+     var us = req.query.username;
+     var pw = req.query.password;
      // var sheetId = '1NuydN_rCsb9X66qVct9YpowCFSVa4D0f6o8e7obAUMQ';
      var sheetTitle = 'taikhoan_khoaphong';
      const user = await getDataApiGgl.kq(sheetTitle);
      // var user = await getData.getDataSheet(sheetTitle,sheetId);
      const soDongTrongSheet = user.data.valueRanges[0].values.length;
-     for (var i = 1; i < soDongTrongSheet; i++) {
-          if(user.data.valueRanges[0].values[i][0] === username && user.data.valueRanges[0].values[i][2] === password)
+     // res.send({ 'trangthai': us, 'tenTaiKhoan': pw });
+     for (var i = 0; i < soDongTrongSheet; i++) {
+          if(user.data.valueRanges[0].values[i][0] == us && user.data.valueRanges[0].values[i][2] == pw)
           {
                trangthai = 1;
                tenTaiKhoan = user.data.valueRanges[0].values[i][1];
                
-          }         
+          }       
+         
      }
-     // var result = { 'trangthai': trangthai, 'tenTaiKhoan': tenTaiKhoan };
+     res.send({ 'trangthai': trangthai, 'tenTaiKhoan': tenTaiKhoan }); 
+     // user.data.valueRanges[0].values.forEach(function (item,index){
+     //      if(item[0] == toString(us) && item [2]== toString(pw))
+     //      {
+     //           trangthai = 1;
+     //           tenTaiKhoan = item [1];
+               
+     //      }  
+     // })
+     
      // res.send({ 'trangthai': trangthai, 'tenTaiKhoan': tenTaiKhoan }); 
-     res.send('check login');
+     // var result = { 'trangthai': trangthai, 'tenTaiKhoan': tenTaiKhoan };
+     // res.send({ 'trangthai': username, 'tenTaiKhoan': password }); 
+     // res.send({'kq' : trangthai});
      
 });
 
