c91f329445f6e0db0dbc07263375d7a2335e7a5e
tony tuan <tuantvp@gmail.com>
2022-10-23 21:19:46 +0700

    them login

--- routes/login.js
+++ routes/login.js
@@ -67,9 +67,9 @@ router.get('/check_loginServer', cors(), async function(req, res, next) {
 });
 
 router.post('/check_loginServer2', cors(), async function(req, res, next) {
-     // res.header("Access-Control-Allow-Origin", "*");
-     // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     // next();
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     next();
      // var sheetId = '1NuydN_rCsb9X66qVct9YpowCFSVa4D0f6o8e7obAUMQ';
      const us = req.body.user;
      const pw = req.body.pass;
