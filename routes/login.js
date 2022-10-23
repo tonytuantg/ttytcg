0de5ab39fd4384d8501635e7a8e70c5ff8d1f627
tony tuan <tuantvp@gmail.com>
2022-10-23 20:17:05 +0700

    them login

--- routes/login.js
+++ routes/login.js
@@ -64,7 +64,7 @@ router.get('/check_loginServer', cors(), async function(req, res, next) {
      
 });
 
-router.post('/check_loginServer2', async function(req, res, next) {
+router.get('/check_loginServer2', async function(req, res, next) {
      // res.header("Access-Control-Allow-Origin", "*");
      // res.header("Access-Control-Allow-Headers", "X-Requested-With");
      // next();
@@ -77,30 +77,15 @@ router.post('/check_loginServer2', async function(req, res, next) {
      const user = await getDataApiGgl.kq(sheetTitle);
      const soDongTrongSheet = user.data.valueRanges[0].values.length;
      
-     for (var i = 1; i <= soDongTrongSheet; i++) {
-          if(user.data.valueRanges[0].values[i][0] == us && user.data.valueRanges[0].values[i][2] == pw)
-          {
-               trangthai = 1;
-               tenTaiKhoan = user.data.valueRanges[0].values[i][1];
-               
-          }       
-         
-     }
-     res.send({ 'trangthai': us, 'tenTaiKhoan': pw }); 
-     // res.send(user.data.valueRanges[0].values); 
-     // user.data.valueRanges[0].values.forEach(function (item,index){
-     //      if(item[0] == toString(us) && item [2]== toString(pw))
+     // for (var i = 1; i <= soDongTrongSheet; i++) {
+     //      if(user.data.valueRanges[0].values[i][0] == us && user.data.valueRanges[0].values[i][2] == pw)
      //      {
      //           trangthai = 1;
-     //           tenTaiKhoan = item [1];
+     //           tenTaiKhoan = user.data.valueRanges[0].values[i][1];
                
-     //      }  
-     // })
-     
-     // res.send({ 'trangthai': trangthai, 'tenTaiKhoan': tenTaiKhoan }); 
-     // var result = { 'trangthai': trangthai, 'tenTaiKhoan': tenTaiKhoan };
-     // res.send({ 'trangthai': username, 'tenTaiKhoan': password }); 
-     // res.send({'kq' : trangthai});
-     
+     //      }       
+         
+     // }
+     res.send({ 'trangthai': 'us', 'tenTaiKhoan': 'pw' }); 
 });
 module.exports = router;
