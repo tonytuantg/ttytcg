fd811e0b5e233455d199fdee565d55b90b02f851
tony tuan <tuantvp@gmail.com>
2022-10-23 22:01:59 +0700

    them login

--- routes/login.js
+++ routes/login.js
@@ -66,47 +66,4 @@ router.get('/check_loginServer', cors(), async function(req, res, next) {
      
 });
 
-router.post('/check_loginServer2', async function(req, res, next) {
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     next();
-     // var sheetId = '1NuydN_rCsb9X66qVct9YpowCFSVa4D0f6o8e7obAUMQ';
-     const us = req.query.user;
-     const pw = req.query.pass;
-     await check(us,pw);
-     if(arr.length >= 1){
-          res.send('1');
-     }else{
-          res.send('0');
-     }
-     
-
-     // user.data.valueRanges[0].values.forEach(function (item,index){
-     //      if(item[0] == 'khth' && item [2] == '123')
-     //      {
-     //           trangthai = 1;
-     //           tenTaiKhoan = item [1];               
-     //      }  
-     // })
-
-     // res.send({ 'trangthai': us, 'tenTaiKhoan': pw }); 
-});
-var arr = [];
-async function check(us,pw){
-     var sheetTitle = 'taikhoan_khoaphong';
-     const user = await getDataApiGgl.kq(sheetTitle);
-     const soDongTrongSheet = user.data.valueRanges[0].values.length;
-     
-     for (var i = 1; i <= soDongTrongSheet; i++) {
-          if(user.data.valueRanges[0].values[i][0] == us && user.data.valueRanges[0].values[i][2] == pw)
-          {
-               // trangthai = 1;
-               arr.push(user.data.valueRanges[0].values[i][1]);
-               // tenTaiKhoan = user.data.valueRanges[0].values[i][1];                            
-          }                
-     }
-     return arr;
-     
-}
-
 module.exports = router;
