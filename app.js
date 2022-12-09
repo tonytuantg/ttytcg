f79965ce59955348a952e81d5528e460fb52d1ed
tonytuantg <120193213+tonytuantg@users.noreply.github.com>
2022-12-09 22:53:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: 'http://ttytcg.epizy.com/',
+  origin: '*',
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
